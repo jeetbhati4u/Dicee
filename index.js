e9adb07f8ef60b066d095b45bb8fe3786db84013
Jitendra Bhati <121750743+jeetbhati4u@users.noreply.github.com>
2023-08-26 18:29:48 +0530

    Update index.js
    Added button functionality

--- index.js
+++ index.js
@@ -30,3 +30,9 @@ else if(randomNumber2 > randomNumber1) {
 else {
     document.querySelector("h1").innerHTML = "🤝 Draw!"
 }
+
+// to click on the button to refresh the page
+
+function refreshPage(){
+  location.reload();
+}
